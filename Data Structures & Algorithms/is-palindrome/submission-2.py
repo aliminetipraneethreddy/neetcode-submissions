a20e13c89f56a7423b1c7819c3ce83f5812aab5e
aliminetipraneethreddy <aliminetipraneethreddy@gmail.com>
2026-06-08 19:15:22 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,26 @@
+import string
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.replace(" ","")
+        s=s.replace('?',"")
+        for ch in string.punctuation:
+            s = s.replace(ch, "")
+        s=s.lower()
+        left=0
+        right=len(s)-1
+        while left < right:
+            # Skip non-alphanumeric characters
+            while left < right and not s[left].isalnum():
+                left += 1
+
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            # Compare characters ignoring case
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
