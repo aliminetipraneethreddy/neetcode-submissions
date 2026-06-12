aba999888c886d6e807db50bab396909e63642c5
aliminetipraneethreddy <aliminetipraneethreddy@gmail.com>
2026-06-12 11:10:34 +0530

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        l1=list(word)
+        Flag=True
+        for i in board:
+            for j in i:
+                if j in l1:
+                    l1.remove(j)
+        if len(l1)==0:
+            return True
+        else:
+            False
+
+
+        
